f0434a0046ceb55eec1c1ba16286e2b76788ed40
Yuliya Zhuravel <zhuravel.dev@gmail.com>
2021-01-23 17:46:30 +0200

    first commit

---
+++ app/src/main/java/com/example/redditapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.redditapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+        RecycleView recycleview = new RecycleView();
+
+
+}
